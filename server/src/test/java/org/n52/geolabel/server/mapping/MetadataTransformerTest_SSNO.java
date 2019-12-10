8d4d7d2c3661de37ae58447e23f6daac3ee04dda
Ani <a_grau05@wwu.de>
2019-12-10 13:25:43 +0100

    add MetadataTransformerTest_SSNO

---
+++ server/src/test/java/org/n52/geolabel/server/mapping/MetadataTransformerTest_SSNO.java
@@ -0,0 +1,27 @@
+package org.n52.geolabel.server.mapping;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.n52.geolabel.commons.Label;
+import org.n52.geolabel.commons.LabelFacet.Availability;
+import org.n52.geolabel.server.config.GeoLabelObjectMapper;
+import org.n52.geolabel.server.config.TransformationDescriptionLoader;
+import org.n52.geolabel.server.config.TransformationDescriptionResources;
+
+public class MetadataTransformerTest_SSNO {
+
+    
+
+}
+
