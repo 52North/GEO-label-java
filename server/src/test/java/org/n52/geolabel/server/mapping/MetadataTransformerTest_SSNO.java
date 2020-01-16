b60cc8ef416294cf0fa98de9231e2c6c4570caa0
Ani <a_grau05@wwu.de>
2020-01-16 18:58:46 +0100

    citation information tests

--- server/src/test/java/org/n52/geolabel/server/mapping/MetadataTransformerTest_SSNO.java
+++ server/src/test/java/org/n52/geolabel/server/mapping/MetadataTransformerTest_SSNO.java
@@ -49,7 +49,7 @@ public class MetadataTransformerTest_SSNO {
     @Test //tests if the given SSNODocuments contains information about the producer
     public void producerProfile() throws IOException {
         //EasyRDFConverter RDF/XML encoding, prov:wasAssociatedWith, prov:Person
-        Label label = testSSNODocument("ssno/ERC_producer_profile_prov_foaf_Person_icecore.rdf");
+        Label label = testSSNODocument("ssno/ERC_producer_profile_Person_icecore.rdf");
 
         assertThat("producer profile is found",
                    label.getProducerProfileFacet().getAvailability(),
@@ -68,7 +68,7 @@ public class MetadataTransformerTest_SSNO {
                    containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
         
         //MyBluemixConverter RDF/XML encoding, prov:wasAttributedTo, prov:Organization
-        label = testSSNODocument("ssno/MBC_producer_profile_prov_foaf_Organization_iphonebarometer.rdf");
+        label = testSSNODocument("ssno/MBC_producer_profile_Organization_iphonebarometer.rdf");
 
         assertThat("producer profile is found",
                     label.getProducerProfileFacet().getAvailability(),
@@ -90,7 +90,7 @@ public class MetadataTransformerTest_SSNO {
     @Test //tests if the given SSNODocuments contains comments of the producer
     public void producerComments() throws IOException {
         //EasyRDFConverter RDF/XML encoding
-        Label label = testSSNODocument("ssno/ERC_producer_comments_rdf_coiloilplant.rdf");
+        Label label = testSSNODocument("ssno/ERC_producer_comments_coiloilplant.rdf");
 
         assertThat("producer comment is found",
                    label.getProducerCommentsFacet().getAvailability(),
@@ -101,7 +101,7 @@ public class MetadataTransformerTest_SSNO {
                    containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
         
         //MyBluemixConverter RDF/XML encoding
-        label = testSSNODocument("ssno/MBC_producer_comments_rdf_coiloilplant.rdf");
+        label = testSSNODocument("ssno/MBC_producer_comments_coiloilplant.rdf");
 
         assertThat("producer comment is found",
                    label.getProducerCommentsFacet().getAvailability(),
@@ -191,28 +191,44 @@ public class MetadataTransformerTest_SSNO {
 
     }
 
-    @Test //tests if the given SSNODocuments contains comments of the producer
+    @Test //tests if the given SSNODocuments contains quality and lineage information
     public void lineageAndQuality() throws IOException {
         //EasyRDFConverter RDF/XML encoding
-        Label label = testSSNODocument("ssno/ERC_lineage_quality_information_sosa_ssn_ip68smartsensor.rdf");
+        Label label = testSSNODocument("ssno/ERC_lineage_quality_information_ip68smartsensor.rdf");
 
         assertThat("Quality information is found",
                     label.getQualityInformationFacet().getAvailability(),
+                    equalTo(Availability.AVAILABLE));
+        
+        assertThat("Lineage information is found",
+                    label.getLineageFacet().getAvailability(),
                     equalTo(Availability.AVAILABLE));
         
         assertThat("Hover-Over is not supported",
                    label.getQualityInformationFacet().getTitle(),
                    containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
+            
+        assertThat("Hover-Over is not supported",
+                   label.getLineageFacet().getTitle(),
+                   containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
         
         //MyBluemixConverter RDF/XML encoding
-        label = testSSNODocument("ssno/MBC_lineage_quality_information_sosa_ssn_dht22description.rdf");
+        label = testSSNODocument("ssno/MBC_lineage_quality_information_dht22description.rdf");
 
         assertThat("Quality information is found",
                     label.getQualityInformationFacet().getAvailability(),
                     equalTo(Availability.AVAILABLE));
+        
+        assertThat("Lineage information is found",
+                    label.getLineageFacet().getAvailability(),
+                    equalTo(Availability.AVAILABLE));
 
         assertThat("Hover-Over is not supported",
                    label.getQualityInformationFacet().getTitle(),
+                   containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
+
+        assertThat("Hover-Over is not supported",
+                   label.getLineageFacet().getTitle(),
                    containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
                 
     }
@@ -220,7 +236,7 @@ public class MetadataTransformerTest_SSNO {
     @Test //tests if the given SSNODocuments contains user feedback or ratings
     public void userFeedback() throws IOException {
         //EasyRDFConverter RDF/XML encoding, duv:RatingFeedback
-        Label label = testSSNODocument("ssno/ERC_user_feedback_rating_dcat_duv_oa_dct_foaf_sunspots.rdf");
+        Label label = testSSNODocument("ssno/ERC_user_feedback_rating_sunspots.rdf");
 
         assertThat("User feedback is found",
                     label.getUserFeedbackFacet().getAvailability(),
@@ -231,7 +247,7 @@ public class MetadataTransformerTest_SSNO {
                    containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
         
         //MyBluemixConverter RDF/XML encoding, duv:UserFeedback
-        label = testSSNODocument("ssno/MBC_user_feedback_feedback_dcat_duv_oa_dct_foaf_sunspots.rdf");
+        label = testSSNODocument("ssno/MBC_user_feedback_feedback_sunspots.rdf");
 
         assertThat("User feedback is found",
                     label.getUserFeedbackFacet().getAvailability(),
@@ -243,7 +259,34 @@ public class MetadataTransformerTest_SSNO {
                 
     }
 
+    @Test //tests if the given SSNODocuments contains citations informations
+    public void citationsInformation() throws IOException {
+        //EasyRDFConverter RDF/XML encoding
+        Label label = testSSNODocument("ssno/ERC_citation_information_spinningcups.rdf");
 
+        assertThat("Citations information is found",
+                    label.getCitationsFacet().getAvailability(),
+                    equalTo(Availability.AVAILABLE));
+        
+        assertThat("Hover-Over is not supported",
+                   label.getCitationsFacet().getTitle(),
+                   containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
+        
+        //MyBluemixConverter RDF/XML encoding
+        label = testSSNODocument("ssno/MBC_citation_information_spinningcups.rdf");
+
+        assertThat("Citations information is found",
+                    label.getCitationsFacet().getAvailability(),
+                    equalTo(Availability.AVAILABLE));
+        
+        assertThat("Hover-Over is not supported",
+                   label.getCitationsFacet().getTitle(),
+                   containsString("Hoverover and drilldown for RDF / XML are not supported yet."));
+                
+    }
+
+
+    
 
     private Label testSSNODocument(String input) throws MalformedURLException, IOException {
         InputStream metadataStream = getClass().getClassLoader().getResourceAsStream(input);
